afce10d40c0475126c3c04d9ffe1275002f6ed2e
Richard Kille <rjkille@gmail.com>
2017-04-04 21:17:14 +0100

    Added core angularJS quickstart components

--- src/goBlog/main.go
+++ src/goBlog/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	log.Println("START")
+
+	http.Handle("/", http.FileServer(http.Dir("./html/")))
+
+	http.ListenAndServe(":80", nil)
 }
